c22abd00030cdef0db7cb4ed2b1fea133bf53862
Cephas-05 <cephasprincely5@gmail.com>
2022-02-20 14:08:22 +0530

    Added new tracking feature

--- model.py
+++ model.py
@@ -1,13 +1,13 @@
 #initialization
 blood_bank = dict()
 blood_bank = {"A+":3, "B+": 10, "A-":2, "O+":1, "O-":2}
+temp = blood_bank
 
 
-
-need_or_want = input("Are you gonna donate or need blood? ")
+need_or_want = input("Are you gonna donate or need or track blood? ")
 if need_or_want == "Donate":
     blood_type = input("Enter your type of blood: ")
-    for i in blood_bank:
+    for i in temp:
         if blood_type == i:
             blood_bank[i] += 1
             print(f"{blood_type} has been added in the blood bank ")
@@ -24,4 +24,13 @@ elif need_or_want == "Need":
     if need not in blood_bank.keys():
         print("Sorry this blood type is not available right now")
 
-        
+
+elif need_or_want == "Track":
+    tracker = input("What blood you want to check?")
+    try:
+        print(blood_bank[tracker])
+    except:
+        print(f"{tracker} is not available in the database")
+
+else:
+    print("Please type either Track or Donate or Need. Don't spell that wrong")
